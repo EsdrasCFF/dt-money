d72a15e3b126f407aab15eef9f2b6076253ddd59
EsdrasCFF <ecff.dev@gmail.com>
2023-10-24 17:14:12 -0300

    Feat: Values formated: Summary and Transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -5,6 +5,7 @@ import { SearchForm } from "./components/SearchForm";
 
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 
 
@@ -25,12 +26,12 @@ export function Transactions () {
                 <tr key={transaction.id} >
                   <td width="50%" >{transaction.description}</td>
                   <td>
-                    <PriceHighLight variant={transaction.type} >
-                      {transaction.price}
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === "outcome" && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighLight>
                   </td>
-                  <td>{transaction.category}</td>
-                  <td>{transaction.created_at}</td>
+                  <td>{ dateFormatter.format(new Date(transaction.created_at))}</td>
                 </tr>
               )
             })}
